e6b4fc6597e753eb9eef0d2855e6130354464256
Matt Cameron <cameron.matt@gmail.com>
2018-01-21 09:43:44 -0600

    datatypes.js - added sumOfTwoNumbers

---
+++ 01week/datatypes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Write a JavaScript program that adds 2 numbers together.
+function sumOfTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log('2 + 2 should equal 4: ', sumOfTwoNumbers(2, 2) === 4);
